9f5884edcf094d50271e7666e4fac38b2f74f61e
Alexandre-Hilaire <alexandre.hilaire@le-campus-numerique.fr>
2023-07-21 16:28:02 +0200

    début d'émélioration de la foncitonalitée feed pour avoir des images

--- scripts/feed.js
+++ scripts/feed.js
@@ -9,7 +9,7 @@ function shows_data(data_pokemons){
 
 
     let pokemons_list = [];
-    let pokemons_url = [];
+    //let pokemons_url = [];
     for (let i = 0; i < data_pokemons.length; i++){
         pokemons_list[i] = data_pokemons[i].name;
         let pokemon_name = document.createElement("p");
@@ -17,6 +17,11 @@ function shows_data(data_pokemons){
         parent_element.appendChild(pokemon_name);
         let pokemons_name_container = document.querySelector("articles");
         pokemon_name.innerHTML = pokemons_list[i];
+        //pokemons_url[i] = data_pokemons[i].url;
+        // fetch(pokemons_url[i])
+        //   .then(response => response.json())
+        //   .then(response => );
+        
     }
     console.log(data_pokemons);
     console.log(3);
@@ -26,9 +31,12 @@ function shows_data(data_pokemons){
 }
 
 console.log(1);
-fetch("https://pokeapi.co/api/v2/pokemon/")
-    .then(response => response.json())
-    .then(response => shows_data(response.results));
+fetch("https://pokeapi.co/api/v2/pokemon/") // Va me chercher les infos à cette url
+    .then(response => response.json()) // mets moi les infos dans un .json
+    .then(response => shows_data(response.results)); 
 
 console.log(2);
 
+fetch("https://pokeapi.co/api/v2/pokemon/1/")
+    .then(response => response.json())
+    .then(response => console.log(response.sprites.other.home.front_default));
