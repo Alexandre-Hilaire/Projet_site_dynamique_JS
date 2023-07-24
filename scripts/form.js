3296d4cd7a54c3a683ab7e71133648c1ff9e524e
Alexandre-Hilaire <alexandre.hilaire@le-campus-numerique.fr>
2023-07-24 11:38:28 +0200

    Feature d'ajout de pokémon dans le feed via le formulaire

--- scripts/form.js
+++ scripts/form.js
@@ -7,4 +7,16 @@ form.addEventListener("submit", (event) => {
     const article_content = document.getElementById("article_content").value; // On récupère la valeur du textarea
     console.log(title); // On affiche les valeurs pour vérifier que tout fonctionne 
     console.log(article_content);
+    // Création de la nouvelle div
+    let parent_element = document.getElementById("pokemon_container"); 
+    let pokemon_container = document.createElement("div");
+    parent_element.appendChild(pokemon_container);
+    // Création du nom du pokémon
+    let pokemon_name = document.createElement("p");
+    pokemon_container.appendChild(pokemon_name);
+    pokemon_name.innerHTML = title;
+    // Création de l'image du pokémon via url
+    let pokemon_img = document.createElement("img");
+    pokemon_img.setAttribute("src", article_content);
+    pokemon_container.appendChild(pokemon_img);
 });
