c9d42d0d0c4443ec80bd06065b83cad3ff9b5a5c
Alexandre-Hilaire <alexandre.hilaire@le-campus-numerique.fr>
2023-07-24 16:26:33 +0200

    Développement de la feature d'ajout d'images en cours

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -2,12 +2,17 @@ const pics = document.getElementById("pics");
 const mozaic_button = document.getElementById("mozaic");
 const col_button = document.getElementById("col");
 
+// Boutons options d'affichage
 function gallery_col (){
     if (pics.style.flexDirection === "row") {
         pics.style.flexDirection = "column";
+        pics.style.justifyContent = "center";
+        pics.style.alignItems = "center";
     }
     else{
         pics.style.flexDirection = "column";
+        pics.style.justifyContent = "center";
+        pics.style.alignItems = "center";
     }
 }
 function gallery_mozaic (){
@@ -15,8 +20,20 @@ function gallery_mozaic (){
         pics.style.flexDirection = "row";
     }
     else{
-        pics.style.flexDirection = "row"
+        pics.style.flexDirection = "row";
     }
 }
 col_button.addEventListener("click", gallery_col);
 mozaic_button.addEventListener("click", gallery_mozaic);
+
+//Feature d'ajout d'image
+function add_img (){
+    
+    let url = prompt("Entrez l\'url de la nouvelle image");
+    let parent_element = document.getElementById("pics")
+    let new_img = document.createElement("img class=pic");
+    new_img.setAttribute("src", url);
+    parent_element.appendChild(new_img);
+
+}
+add_pics.addEventListener("click", add_img);
